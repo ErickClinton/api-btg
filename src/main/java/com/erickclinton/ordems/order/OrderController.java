88ab46c453c730c4ecede931cdf34dfb9540fa75
Erick Clinton <erickleite2009321@gmail.com>
2024-08-15 14:47:28 -0300

    feat: create service for sum all items

--- src/main/java/com/erickclinton/ordems/order/OrderController.java
+++ src/main/java/com/erickclinton/ordems/order/OrderController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class OrderController {
 
@@ -26,7 +28,9 @@ public class OrderController {
                                                                  @RequestParam(name = "pageSize",defaultValue = "10") Integer pageSize){
 
         var pageResponse = this.orderService.findAllByCustomerId(customerId, PageRequest.of(page,pageSize));
+        var totalOnOrders = this.orderService.findTotalOnOrdersByCustomerId(customerId);
         return ResponseEntity.ok(new ApiResponse<>(
+                Map.of("totalOnOrders", totalOnOrders),
                 pageResponse.getContent(),
                 PaginationResponse.fromPage(pageResponse)
         ));
--- src/main/java/com/erickclinton/ordems/order/OrderService.java
+++ src/main/java/com/erickclinton/ordems/order/OrderService.java
@@ -5,20 +5,28 @@ import com.erickclinton.ordems.order.dto.OrderResponse;
 import com.erickclinton.ordems.order.entities.OrderEntity;
 import com.erickclinton.ordems.order.entities.OrderItemEntity;
 import com.erickclinton.ordems.order.repository.OrderRepository;
+import org.bson.Document;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.group;
+
 @Service
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final MongoTemplate mongoTemplate;
 
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository,MongoTemplate mongoTemplate) {
         this.orderRepository = orderRepository;
+        this.mongoTemplate = mongoTemplate;
     }
 
     public void save(OrderCreatedEventDto orderCreatedEventDto) {
@@ -30,6 +38,17 @@ public class OrderService {
         var orders = this.orderRepository.findAllByCustomerId(customerId,pageRequest);
 
         return orders.map(OrderResponse::fromEntity);
+    }
+
+    public BigDecimal findTotalOnOrdersByCustomerId(Long customerId){
+        var aggregations = Aggregation.newAggregation(
+                Aggregation.match(Criteria.where("customerId").is(customerId)),
+                group().sum("total").as("total")
+        );
+
+        var response = mongoTemplate.aggregate(aggregations, "orders", Document.class);
+
+        return new BigDecimal(response.getUniqueMappedResult().get("total").toString());
     }
 
     private static List<OrderItemEntity> getOrderItems(OrderCreatedEventDto orderCreatedEventDto){
